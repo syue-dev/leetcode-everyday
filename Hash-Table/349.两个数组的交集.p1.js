b4d7a5c765ed0bd1c33f0ef2babaaa5ab986c181
286506460 <286506460@qq.com>
2020-11-02 09:43:07 +0800

    20201102

---
+++ Hash-Table/349.两个数组的交集.p1.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=349 lang=javascript
+ *
+ * [349] 两个数组的交集
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+// var intersection = function (nums1, nums2) {
+// 	let temp = {}
+// 	return nums1.filter((item) => {
+// 		if (nums2.includes(item) && !temp[item]) {
+// 			temp[item] = true
+// 			return nums2.includes(item)
+// 		}
+// 	})
+// }
+var intersection = function (nums1, nums2) {
+	if (nums1.length > nums2.length) [nums1, nums2] = [nums2, nums1]
+	let hash = new Set(nums1)
+	let res = new Set()
+	for (let i = 0; i < nums2.length; i++) {
+		if (hash.has(nums2[i])) {
+			res.add(nums2[i])
+		}
+	}
+	return [...res]
+}
+// @lc code=end
